05dc9110573124951479d34d04d39a86b6bfb430
alzerjawi <halzerjawi21@gmail.com>
2023-04-01 21:20:39 +0300

    Updates

---
+++ precache-manifest.483da6e82f816e8f6fcca9786dce42f4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "59a84784e329587734cf",
+    "url": "/static/css/main.42ff09d2.chunk.css"
+  },
+  {
+    "revision": "59a84784e329587734cf",
+    "url": "/static/js/main.59a84784.chunk.js"
+  },
+  {
+    "revision": "e8e3b76c5bf759905946",
+    "url": "/static/js/1.e8e3b76c.chunk.js"
+  },
+  {
+    "revision": "3d3961938546f8a2b8dd",
+    "url": "/static/css/2.427fe948.chunk.css"
+  },
+  {
+    "revision": "3d3961938546f8a2b8dd",
+    "url": "/static/js/2.3d396193.chunk.js"
+  },
+  {
+    "revision": "316baba7ca5f9e773de3",
+    "url": "/static/js/runtime~main.316baba7.js"
+  },
+  {
+    "revision": "7b5bf3e84b68bdf1ee679c24a9622ade",
+    "url": "/static/media/hassan_spain.7b5bf3e8.jpg"
+  },
+  {
+    "revision": "830ecf8e2c44f1931cf227cfb18cce9d",
+    "url": "/static/media/MSU.830ecf8e.png"
+  },
+  {
+    "revision": "a2ea5aea90a3e74493a6266a4971e413",
+    "url": "/static/media/westminster.a2ea5aea.png"
+  },
+  {
+    "revision": "dff82f923f7775b68ff8e06c9b356b69",
+    "url": "/static/media/swisspro.dff82f92.png"
+  },
+  {
+    "revision": "b017941b024ff00b6c9d7d9fddc37c3c",
+    "url": "/static/media/Al-Turath.b017941b.png"
+  },
+  {
+    "revision": "6c1fb76adc326f71b4104e0d1d7f2818",
+    "url": "/static/media/construction.6c1fb76a.png"
+  },
+  {
+    "revision": "e132ecbdccda500477dec5f983673815",
+    "url": "/index.html"
+  }
+];
